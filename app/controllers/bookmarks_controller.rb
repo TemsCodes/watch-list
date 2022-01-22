c7e8868b2078b272bc56b75dcaed13de2996893e
Temi Adaramewa <miss.t.adaramewa@gmail.com>
2022-01-22 16:15:12 +0000

    pages complete

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+    before_action :find_list, only: [:create]
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmark_path(@bookmark)
+  end
+    
+end
+
+private
+
+ def find_list
+    @list = List.find(params[:list_id])
+ end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end 
